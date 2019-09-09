eb008d73d7788769d6b0431071bd9a30c28ae180
ericbanker12 <ericbanker12@gmail.com>
2019-09-09 14:48:28 -0500

    add team name parameters

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
-function App() {
+function App(props) {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0)
   const [awayScore, setAwayScore] = useState(0)
@@ -13,7 +13,7 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">{props.home}</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -21,7 +21,7 @@ function App() {
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">{props.away}</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
